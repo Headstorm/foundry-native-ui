baf86ebf617ea2551d9a00b09cc2b7ec554a01e7
Eric <24817191+ericbaumann@users.noreply.github.com>
2021-07-07 19:13:46 -0400

    update the API and only add text-children to styled text container
    address PR comments
    
    removed unused import

--- src/components/commonTypes.ts
+++ src/components/commonTypes.ts
@@ -1,1 +1,9 @@
+import { StyledComponentBase } from 'styled-components/native';
+
 export type SubcomponentPropsType = Record<string, unknown>;
+
+export type SubcomponentProperties = {
+  component?: StyledComponentBase<any, {}>;
+  props?: SubcomponentPropsType;
+  ref?: React.RefObject<any>;
+};
