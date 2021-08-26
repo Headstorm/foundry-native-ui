cd83970fb3ad4bc4c23868b3006b57d2f53743ca
Dawson Booth <github@dawsonbooth.com>
2021-08-26 16:17:26 -0500

    #41 Fix errors in Modal story

--- storybook/stories/Modal/Modal.stories.js
+++ storybook/stories/Modal/Modal.stories.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components/native';
+
+import { ImageBackground } from 'react-native';
+
 import { boolean, select, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
@@ -9,77 +11,79 @@ import Modal from '../../../src/components/Modal';
 import Button from '../../../src/components/Button';
 import Card from '../../../src/components/Card';
 
-const Background = styled.View`
-  background-image: url(https://source.unsplash.com/weekly?landscape);
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const DefaultModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
 
-  height: 100vh;
-  width: 100vw;
-`;
+  const handleClose = () => {
+    setIsOpen(false);
+    action('close')();
+  };
+
+  const buttonAttachment = select(
+    'closeButtonAttachment',
+    ['inside', 'outside', 'corner'],
+    'inside',
+  );
+
+  return (
+    <>
+      <Card elevation={1} header="Use this button to open the modal again">
+        <Button
+          color={colors.primaryDark}
+          StyledContainer={Button.Container}
+          onClick={() => setIsOpen(true)}
+        >
+          Open modal
+        </Button>
+      </Card>
+      {isOpen && (
+        <Modal
+          closeButtonAttachment={buttonAttachment}
+          backgroundDarkness={number('backgroundDarkness', 0.5, {
+            range: true,
+            min: 0,
+            max: 1,
+            step: 0.05,
+          })}
+          backgroundBlur={`${number('backgroundBlur', 0.5, {
+            range: true,
+            min: 0,
+            max: 5,
+            step: 0.1,
+          })}rem`}
+          onClickOutside={boolean('onClickOutside function', true) ? handleClose : undefined}
+          onClose={handleClose}
+        >
+          <Card
+            header="Hello world!"
+            footer={
+              <Button color={colors.primaryDark} onClick={handleClose}>
+                Okay...
+              </Button>
+            }
+            elevation={1}
+          >
+            The content of the modal (the card and everything inside it) is customizable. The close
+            &times; is built-in but can be easily overwritten. It is the very model of a modern
+            major React modal.
+          </Card>
+        </Modal>
+      )}
+    </>
+  );
+};
 
 storiesOf('Modal', module)
   .addParameters({ component: Modal })
-  .add('Default', () => {
-    const [isOpen, setIsOpen] = useState(true);
-
-    const handleClose = () => {
-      setIsOpen(false);
-      action('close')();
-    };
-
-    const buttonAttachment = select(
-      'closeButtonAttachment',
-      ['inside', 'outside', 'corner'],
-      'inside',
-    );
-
-    return (
-      <Background>
-        <Card elevation={1} header="Use this button to open the modal again">
-          <Button
-            color={colors.primaryDark}
-            StyledContainer={Button.Container}
-            onClick={() => setIsOpen(true)}
-          >
-            Open modal
-          </Button>
-        </Card>
-        {isOpen && (
-          <Modal
-            closeButtonAttachment={buttonAttachment}
-            backgroundDarkness={number('backgroundDarkness', 0.5, {
-              range: true,
-              min: 0,
-              max: 1,
-              step: 0.05,
-            })}
-            backgroundBlur={`${number('backgroundBlur', 0.5, {
-              range: true,
-              min: 0,
-              max: 5,
-              step: 0.1,
-            })}rem`}
-            onClickOutside={boolean('onClickOutside function', true) ? handleClose : undefined}
-            onClose={handleClose}
-          >
-            <Card
-              header="Hello world!"
-              footer={
-                <Button color={colors.primaryDark} onClick={handleClose}>
-                  Okay...
-                </Button>
-              }
-              elevation={1}
-            >
-              The content of the modal (the card and everything inside it) is customizable. The
-              close &times; is built-in but can be easily overwritten. It is the very model of a
-              modern major React modal.
-            </Card>
-          </Modal>
-        )}
-      </Background>
-    );
-  });
+  .addDecorator(getStory => (
+    <ImageBackground
+      source={{ uri: 'https://source.unsplash.com/weekly?landscape' }}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      {getStory()}
+    </ImageBackground>
+  ))
+  .add('Default', () => <DefaultModal />);
